e037fc995e30b33d230b3402d48fab93b5e79bc4
renyifan <renyifan@iairfly.com>
2018-11-23 15:33:02 +0800

    [add] util

---
+++ util.c
@@ -0,0 +1,119 @@
+#include "util.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//字符出现的次数
+int countChar(char * str, char c)
+{
+    int i = 0,count=0;
+    for(i=0;i<strlen(str);i++)
+        if (str[i] == c)
+            count++;
+    return count;
+}
+
+//字符第一次出现的位置
+int indexOfStrFirst(char * str, char c)
+{
+    int i = 0;
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] == c)
+            return i;
+    }
+    return -1;
+}
+
+//在特殊符号处截取字符
+char *getFileType(const char *filename) { //根据扩展名返回文件类型描述  
+    char sExt[32];
+    const char *p_start = filename;
+    memset(sExt, 0, sizeof(sExt));
+    while (*p_start)
+    {
+        if (*p_start == '.')
+        {
+            p_start++;
+            strncpy(sExt, p_start, sizeof(sExt));
+            break;
+        }
+        p_start++;
+    }
+
+    if (strncmp(sExt, "bmp", 3) == 0)
+        return "image/bmp";
+
+    if (strncmp(sExt, "gif", 3) == 0)
+        return "image/gif";
+
+    if (strncmp(sExt, "ico", 3) == 0)
+        return "image/x-icon";
+
+    if (strncmp(sExt, "jpg", 3) == 0)
+        return "image/jpeg";
+
+    if (strncmp(sExt, "avi", 3) == 0)
+        return "video/avi";
+
+    if (strncmp(sExt, "css", 3) == 0)
+        return "text/css";
+
+    if (strncmp(sExt, "dll", 3) == 0)
+        return "application/x-msdownload";
+
+    if (strncmp(sExt, "exe", 3) == 0)
+        return "application/x-msdownload";
+
+    if (strncmp(sExt, "dtd", 3) == 0)
+        return "text/xml";
+
+    if (strncmp(sExt, "mp3", 3) == 0)
+        return "audio/mp3";
+
+    if (strncmp(sExt, "mpg", 3) == 0)
+        return "video/mpg";
+
+    if (strncmp(sExt, "png", 3) == 0)
+        return "image/png";
+
+    if (strncmp(sExt, "ppt", 3) == 0)
+        return "application/vnd.ms-powerpoint";
+
+    if (strncmp(sExt, "xls", 3) == 0)
+        return "application/vnd.ms-excel";
+
+    if (strncmp(sExt, "doc", 3) == 0)
+        return "application/msword";
+
+    if (strncmp(sExt, "mp4", 3) == 0)
+        return "video/mpeg4";
+
+    if (strncmp(sExt, "ppt", 3) == 0)
+        return "application/x-ppt";
+
+    if (strncmp(sExt, "wma", 3) == 0)
+        return "audio/x-ms-wma";
+
+    if (strncmp(sExt, "wmv", 3) == 0)
+        return "video/x-ms-wmv";
+
+    return "text/html";
+}
+
+//获取文件大小
+int getFileSize(const char * filename)
+{
+    int fileSize = 0;
+    FILE *fp = NULL;
+    if (filename == NULL)
+        return 0;
+    //以二进制的形式读取
+    fp = fopen(filename, "rb");
+    if (fp == NULL)
+        return 0;
+    fseek(fp, 0, SEEK_END);
+    fileSize = ftell(fp);
+    rewind(fp);
+    fclose(fp);
+    return fileSize;
+}
